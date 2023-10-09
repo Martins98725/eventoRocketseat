380b7a93e89e4052dd1f2f48b3dbe6701d83616c
Martins98725 <icaro25martins@gmail.com>
2023-10-09 10:31:26 -0300

    Revert "Revert "coisas basicas do evento""
    This reverts commit 8af4518293677d471f10edea485273593105239b.

---
+++ src/test/java/br/icaromartins/todolisticaro/TodolistIcaroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.icaromartins.todolisticaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistIcaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
